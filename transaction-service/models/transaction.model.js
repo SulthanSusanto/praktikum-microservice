96414e9d387e4042237783c3e5871f3b72135cf1
sulthan susanto <sulthanltsusanto@gmail.com>
2025-08-01 07:09:45 +0800

    fix things

--- transaction-service/models/transaction.model.js
+++ transaction-service/models/transaction.model.js
@@ -2,6 +2,8 @@ module.exports = (sequelize, DataTypes) => {
   const Transaction = sequelize.define('Transaction', {
     quantity: DataTypes.INTEGER,
     totalPrice: DataTypes.INTEGER,
+    userId: DataTypes.INTEGER,
+    productId: DataTypes.INTEGER,
   });
 
   return Transaction;
